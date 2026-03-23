a9aa885578f070340a91f4cb73ebd1116cfcd187
jhamile4 <jhamile.macavilca@tecsup.edu.pe>
2026-03-23 12:44:04 -0500

    COMMIT FINAL

--- ejercicio3.playground/Contents.swift
+++ ejercicio3.playground/Contents.swift
@@ -1,38 +1,36 @@
+
 import Foundation
 func funcB(_ nombre: String) -> Bool {
     return nombre == "UNI"
 }
-
 func funcC(_ nombre: String) -> Bool {
-    return nombre == "UNALM"
+    return nombre == "UNAL"
 }
-
 func funcD(_ nombre: String) -> Bool {
-    return nombre == "UPN"
+    return nombre == "UPM"
 }
-
-func funcA(_ nombre: String, _ maxLlamadas: Int) {
+func funcA(_ nombre: String, _ intentosRestantes: Int) {
+    
+    if intentosRestantes == 0 {
+        print("No se encontró '\(nombre)'. Se superó el límite de intentos.")
+        return
+    }
     if nombre == "TECSUP" || nombre == "MIT" {
         print("Encontrado '\(nombre)' en FUNC A")
         return
     }
-    
-    if maxLlamadas >= 2 && funcB(nombre) {
+    if intentosRestantes == 2 && funcB(nombre) {
         print("Encontrado '\(nombre)' en FUNC B")
         return
     }
-    
-    if maxLlamadas >= 3 && funcC(nombre) {
+    if intentosRestantes == 1 && funcC(nombre) {
         print("Encontrado '\(nombre)' en FUNC C")
         return
     }
     
-    if maxLlamadas >= 4 && funcD(nombre) {
-        print("Encontrado '\(nombre)' en FUNC D")
-        return
-    }
-    
-    print("No se encontro '\(nombre)'. Se supero el limite de \(maxLlamadas) llamadas.")
+    funcA(nombre, intentosRestantes - 1)
 }
-funcA("UPN", 2)
-funcA("TECSUP,2)
+funcA("UPM", 2)
+funcA("UNI", 2)
+funcA("TECSUP", 2)
+funcA("UNAL", 2)
