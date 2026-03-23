fc1f13ee34b97bb79dfaaa9e4faa079f3144db3d
jhamile4 <jhamile.macavilca@tecsup.edu.pe>
2026-03-23 12:37:18 -0500

    Update function C and print statements in funcA

--- ejercicio3.playground/Contents.swift
+++ ejercicio3.playground/Contents.swift
@@ -4,7 +4,7 @@ func funcB(_ nombre: String) -> Bool {
 }
 
 func funcC(_ nombre: String) -> Bool {
-    return nombre == "UNAL"
+    return nombre == "UNALM"
 }
 
 func funcD(_ nombre: String) -> Bool {
@@ -18,20 +18,21 @@ func funcA(_ nombre: String, _ maxLlamadas: Int) {
     }
     
     if maxLlamadas >= 2 && funcB(nombre) {
-        print("Encontrado '\(nombre)' en FUNC A")
+        print("Encontrado '\(nombre)' en FUNC B")
         return
     }
     
     if maxLlamadas >= 3 && funcC(nombre) {
-        print("Encontrado '\(nombre)' en FUNC A")
+        print("Encontrado '\(nombre)' en FUNC C")
         return
     }
     
     if maxLlamadas >= 4 && funcD(nombre) {
-        print("Encontrado '\(nombre)' en FUNC A")
+        print("Encontrado '\(nombre)' en FUNC D")
         return
     }
     
     print("No se encontro '\(nombre)'. Se supero el limite de \(maxLlamadas) llamadas.")
 }
 funcA("UPN", 2)
+funcA("TECSUP,2)
